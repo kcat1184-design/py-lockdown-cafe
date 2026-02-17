ed5621ba236952007ce88cf45535ad8fa0c7f18d
mac <mac@MacBook-Air-mac.local>
2026-02-17 16:34:04 +0200

    solution

--- app/cafe.py
+++ app/cafe.py
@@ -13,7 +13,6 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
 
-
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Visitor is not vaccinated")
 
@@ -22,9 +21,7 @@ class Cafe:
         if expiration_date < datetime.date.today():
             raise OutdatedVaccineError("Vaccine is outdated")
 
-
         if not visitor.get("wearing_a_mask", False):
             raise NotWearingMaskError("Visitor is not wearing a mask")
-
 
         return f"Welcome to {self.name}"
