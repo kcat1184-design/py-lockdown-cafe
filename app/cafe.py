6a9590dd04763e24e93e44bc34bf979ac00ed4a3
mac <mac@MacBook-Air-mac.local>
2026-02-18 13:36:19 +0200

    develop

--- app/cafe.py
+++ app/cafe.py
@@ -8,7 +8,7 @@ from app.errors import (
 
 class Cafe:
 
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: None) -> None:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
